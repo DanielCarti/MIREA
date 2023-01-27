25d8227ec912e6ebd739b865d63f2d75543ab36c
Daniel <dmegenskiy@mail.ru>
2023-01-28 02:27:14 +0300

    Version2.2

--- BGN/kurs.py
+++ BGN/kurs.py
@@ -6,7 +6,7 @@ import seaborn as sns
 
 
 def kurs_lev(days = 16, color = 'purple'):
-    kurs = pd.read_csv('C:\\Users\\Daniel\\Documents\\MIREA\\BGN\\kurs1.csv', index_col=0)
+    kurs = pd.read_csv('C:\\Users\\Daniel\\Documents\\KursachProject\\MIREA\\BGN\\kurs1.csv', index_col=0)
     kurs.head()
     days = int(days)
     color = str(color)
@@ -16,7 +16,7 @@ def kurs_lev(days = 16, color = 'purple'):
     sns.lineplot(x=number_days, y=name_of_row, color=color)
     plt.legend(labels=["Курс BGN/RUB"], title="y=BGN/RUB, x=Days")
     ts = str(time.time()).replace('.', '')
-    image_path = f"C:\\Users\\Daniel\\Documents\\MIREA\\Images\\kurs_levdata{ts}.png"
+    image_path = f"C:\\Users\\Daniel\\Documents\\KursachProject\\MIREA\\Images\\kurs_levdata{ts}.png"
     plt.savefig(f'{image_path}')
     return image_path
     print(kurs_lev())
